0303ee788081482bcf94cfb8971a5150239ffbba
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2022-06-13 00:13:01 +0530

    setup UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
+import React from "react";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
+import { data } from "../data";
+
 function App() {
   return (
     <div className="App">
-      Hello
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">
+            Movies
+          </div>
+          <div className="tab">
+            Favourites
+          </div>
+        </div>
+        <div className="list">
+          {data.map(movie => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
