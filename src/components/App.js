0e0e09dd18f9ed848c8433648862a8776648ed89
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2022-06-13 11:14:50 +0530

    setup listener to store

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,46 @@ import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
 import { data } from "../data";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">
-            Movies
+class App extends React.Component {
+  componentDidMount() {
+    const { store } = this.props;
+    // sub to state changes
+    store.subscribe(() => {
+      this.forceUpdate();
+    });
+
+    // get data, dispatch action
+    store.dispatch({
+      type: "ADD_MOVIES",
+      movies: data
+    });
+
+    console.log(this.props.store.getState());
+  }
+
+  render() {
+    const movies = this.props.store.getState();
+    return (
+      <div className="App" >
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">
+              Movies
+            </div>
+            <div className="tab">
+              Favourites
+            </div>
           </div>
-          <div className="tab">
-            Favourites
+          <div className="list">
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
           </div>
-        </div>
-        <div className="list">
-          {data.map(movie => (
-            <MovieCard movie={movie} />
-          ))}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+import App from './components/App';
+import './index.css';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
 
