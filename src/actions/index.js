fb461d9206bea38ff19d6728e9cfc67edd1ffdfc
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2022-06-13 13:13:08 +0530

    setup favourite movie

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,18 @@
 // ACTION TYPES:
-export const ADD_MOVIES = 'ADD_MOVIES'
+export const ADD_MOVIES = 'ADD_MOVIES';
+export const ADD_FAVOURITES = 'ADD_FAVOURITES';
 
 // ACTION CREATORS
 export function addMovies(movies) {
     return {
         type: ADD_MOVIES,
         movies
+    }
+};
+
+export function addFavourite(movie) {
+    return {
+        type: ADD_FAVOURITES,
+        movie
     }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -18,8 +18,18 @@ class App extends React.Component {
     console.log(this.props.store.getState());
   }
 
+  isMovieFavourite = (movie) => {
+    const { favourites } = this.props.store.getState();
+    const index = favourites.indexOf(movie);
+    if (index !== -1) {
+      return true;
+    }
+    return false;
+  }
+
   render() {
-    const movies = this.props.store.getState();
+    console.log(this.props.store.getState());
+    const { list } = this.props.store.getState();
     return (
       <div className="App" >
         <Navbar />
@@ -33,8 +43,12 @@ class App extends React.Component {
             </div>
           </div>
           <div className="list">
-            {movies.map((movie, index) => (
-              <MovieCard movie={movie} key={`movies-${index}`} />
+            {list.map((movie, index) => (
+              <MovieCard
+                movie={movie}
+                key={`movies-${index}`}
+                dispatch={this.props.store.dispatch}
+                isFavourite={this.isMovieFavourite(movie)} />
             ))}
           </div>
         </div>
